6fa12cc5edc0481f7c16bd7f4fbb8f3ef8520f6f
Kenenbek R <kenenbek@gmail.com>
2021-10-06 00:17:28 +0300

    softmax EMD loss

--- models/losses.py
+++ models/losses.py
@@ -17,6 +17,7 @@ def CrossEntropyLoss(y_pred, y_true):
 
 
 def EMD_squared_loss(n_class, y_pred, y_true):
+    y_pred = F.softmax(y_pred, dim=-1)
     y_true = F.one_hot(y_true, n_class).float()
     output = torch.mean(torch.square(torch.cumsum(y_pred, dim=-1) - torch.cumsum(y_true, dim=-1)))
     return output
