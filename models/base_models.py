fix kl div log space

models/base_models.py
@@ -38,13 +38,12 @@ class CategoricalModel(BaseModel):
         Y_pred = F.softmax(logits, dim=-1)
         Y_pred = Y_pred.squeeze(0)
         Y = y_batch.squeeze(0)
-        
         step = 32768
         # print(X_batch.squeeze(0))
         for j in range(0, len(Y), step):
             f = make_coalescent_heatmap("", (Y_pred[j:j + step].T, Y[j:j + step]))
             plt.show()
-        return
+        return Y_pred
 
 
 class OrdinalModel(BaseModel):
@@ -145,3 +144,11 @@ class ConvEmbedding(LightningModule):
         
         output = output.permute(0, 2, 1)
         return output
+
+
+class NoEmbedding(LightningModule):
+    def __init__(self):
+        super(NoEmbedding, self).__init__()
+    
+    def forward(self, X):
+        return X.unsqueeze(2).float()

models/gru.py
@@ -9,11 +9,11 @@ from .losses import KLDivLoss, CrossEntropyLoss, EMD_squared_loss, CTC_loss, MYL
 
 
 class GruLabeler(base_models.CategoricalModel):
-    def __init__(self, embedding, n_class, hidden_size, num_layers, predictor, device):
+    def __init__(self, embedding, n_class, input_size, hidden_size, num_layers, predictor, device):
         super().__init__()
         self.n_class = n_class
         self.embedding = embedding
-        self.gru = nn.GRU(input_size=hidden_size,
+        self.gru = nn.GRU(input_size=input_size,
                           hidden_size=hidden_size,
                           num_layers=num_layers,
                           bidirectional=True,
@@ -26,8 +26,8 @@ class GruLabeler(base_models.CategoricalModel):
         self.loss = functools.partial(KLDivLoss, n_class)
     
     def forward(self, X):
-        output = self.embedding(X)
-        output, _ = self.gru(output)
+        X = self.embedding(X)
+        output, _ = self.gru(X)
         output = self.predictor(output)
         return output
     

models/losses.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 
 def KLDivLoss(n_class, y_pred, y_true):
     assert len(y_true.shape) == 2
-    y_pred = F.softmax(y_pred, dim=-1)
+    y_pred = F.log_softmax(y_pred, dim=-1)
     y_true = F.one_hot(y_true, n_class).float()
     loss = F.kl_div(y_pred, y_true, reduction='batchmean')
     return loss