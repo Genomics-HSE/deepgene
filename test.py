4969c3a5c383980164e143718e34caaec667fe01
Kenenbek R <kenenbek@gmail.com>
2022-04-16 22:38:31 +0600

    test

--- test.py
+++ test.py
@@ -8,16 +8,15 @@ from tqdm import tqdm
 
 
 if __name__ == '__main__':
-    gin.parse_config_file('configs/big_gru.gin')
-    #gru_model = GruLabeler()
     gru_model = GruLabeler.load_from_checkpoint(checkpoint_path="output_ms/GRU.ckpt").eval()
     
-    for i in tqdm(range(100)):
-        filename = "to_compare/" + str(i) + ".txt"
-        genome = open(filename).read().strip()
-        a = [int(num) for num in list(genome)]
-        input_genome = torch.FloatTensor(a).unsqueeze(0)
-
-        out = gru_model(input_genome)
-        out.squeeze(0)
-        torch.save("output_ms/to_compare_results/" + str(i) + ".pt", out)
+    with torch.no_grad():
+        for i in tqdm(range(100)):
+            filename = "to_compare/" + str(i) + ".txt"
+            genome = open(filename).read().strip()
+            a = [int(num) for num in list(genome)]
+            input_genome = torch.FloatTensor(a).unsqueeze(0)
+    
+            out = gru_model(input_genome)
+            out.squeeze(0)
+            torch.save("output_ms/to_compare_results/" + str(i) + ".pt", out)
